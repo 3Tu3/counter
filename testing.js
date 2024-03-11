b6cf2269d55e8b5560e336658632744accc24bda
3Tu3 <132224447+3Tu3@users.noreply.github.com>
2024-03-11 18:03:52 -0300

    Update testing.js

--- testing.js
+++ testing.js
@@ -1,41 +1,78 @@
 let count = 0;
+let clicks = 0
 
 function updateCounter() {
         document.getElementById("counter").innerText = count;
 
 }
 
-function increment() {
-        count++;
-        updateCounter();
+function updateClicks() {
+    document.getElementById("clicks").innerText = "you clicked " + clicks, "times";
 }
 
-function decrement() {
-    count--;
-    updateCounter();
-}
-
-function more() {
-    count = count + 100;
-    updateCounter();
-}
-
-function less() {
-    count = count - 100;
-    updateCounter();
-}
-
-function fivemore() {
-    count = count + 500;
-    updateCounter();
-}
-
-function fiveless() {
-    count = count - 500;
-    updateCounter();
+function input() {
+    const inside = document.getElementById("inputNumber").value;
+    count = inside;
+    document.getElementById("counter").innerText = count;
 }
 
 function zero() {
     count = 0;
     updateCounter();
+    clicks ++;
+    updateClicks();
+}
+
+function zeroClicks() {
+    clicks = 0;
+    updateClicks();
+}
+
+function zeroAll() {
+    count = 0;
+    updateCounter();
+    clicks = 0;
+    updateClicks();
+}
+
+function increment() {
+        count++;
+        updateCounter();
+        clicks ++;
+        updateClicks();
+}
+
+function decrement() {
+    count--;
+    updateCounter();
+    clicks ++;
+    updateClicks();
+}
+
+function more() {
+    count = count + 100;
+    updateCounter();
+    clicks ++;
+    updateClicks();
+}
+
+function less() {
+    count = count - 100;
+    updateCounter();
+    clicks ++;
+    updateClicks();
+}
+
+function fivemore() {
+    count = count + 500;
+    updateCounter();
+    clicks ++;
+    updateClicks();
+}
+
+function fiveless() {
+    count = count - 500;
+    updateCounter();
+    clicks ++;
+    updateClicks();
 }
